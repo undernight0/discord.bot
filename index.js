77f7def84ac6a254dc5a6b4dde2e26e2ea76c2be
undernight0 <akarsufurkan152@gmail.com>
2026-02-16 20:19:09 +0300

    First bot

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
+
+client.once('ready', () => {
+  console.log(`Bot hazır: ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('Pong! 🏓');
+  }
+});
+
+client.login(process.env.TOKEN);
